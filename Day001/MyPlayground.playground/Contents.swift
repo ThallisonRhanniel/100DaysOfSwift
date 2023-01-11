d0fdbce6207ca0b23c03f416a2328a003b6628c8
Thallison Rhanniel Garcia Lima <thallisonrlima@gmail.com>
2023-01-11 16:54:29 -0300

    Add samples of type annotations

---
+++ Day001/MyPlayground.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "Alterando valor"
+
+var age = 28
+var population = 8_000_000
+
+
+var str1 = """
+Texto
+Texto 2
+Texto 3
+"""
+
+var str2 = """
+Texto \
+Texto2\
+Texto 3
+"""
+
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+var text1 = "Hello my little friend"
+var stringResult = "My result is \(text1)"
+
+//Constants
+
+let MyName = "Thallison"
+
+let Album: String = "Reputation"
+let year: Int = 1989
+let Height: Double = 1.78
+var isMarried: Bool = true
